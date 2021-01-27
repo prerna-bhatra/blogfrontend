9f3ffcdb8063b2e917054d8090eb8c18a60ed419
unknown <bhatraprerna061@gmail.com>
2021-01-27 18:25:08 +0530

    bug in finger print

--- ReadBlog.js
+++ ReadBlog.js
@@ -28,14 +28,14 @@ function ReadBlog()
       // Print the 32bit hash id to the console
       console.log(fingerprint);
    
-    const FindBlogIndex = Object.keys(localStorage)[Object.values(localStorage).indexOf(BlogId)];
-    //console.log( FindBlogIndex)
-    if(FindBlogIndex==undefined)
-    {
-      ViewedValue=1;
-      localStorage.setItem(`layer${localStorage.length+1}`,BlogId)
-    }
-    console.log(ViewedValue)
+    // const FindBlogIndex = Object.keys(localStorage)[Object.values(localStorage).indexOf(BlogId)];
+    // //console.log( FindBlogIndex)
+    // if(FindBlogIndex==undefined)
+    // {
+    //   ViewedValue=1;
+    //   localStorage.setItem(`layer${localStorage.length+1}`,BlogId)
+    // }
+    // console.log(ViewedValue)
     
     
     fetch(`https://desolate-sierra-34755.herokuapp.com/api/ReadBlog/${BlogId}/${fingerprint}`)
